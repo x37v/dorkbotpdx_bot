28c16695f52976f49865ee882950818e5cae5c2a
Jason Plumb <jason@noisybox.net>
2013-03-31 20:26:00 -0700

    Extract method for doing url colorization

--- maxwell.rb
+++ maxwell.rb
@@ -13,28 +13,32 @@ bot = create_bot(opts)
 urls_color = :lime
 
 bot.on :message, /^!(paul|teensy|pjrc)/ do |m|
-	response = Format("Paul [teensy]'s website is %s and he can be reached at %s" % [Format(urls_color, "https://www.pjrc.com"), Format(:yellow, "paul@pjrc.com")])
+	response = Format("Paul [teensy]'s website is %s and he can be reached at %s" % [urlize("https://www.pjrc.com"), Format(:yellow, "paul@pjrc.com")])
 	m.reply(response)
 end
 
 bot.on :message, /^!(laen|osh|pcb)/ do |m|
-	response = Format("Laen's PCB order has its own channel, %s and the website is %s" % [Format(:yellow, "#oshpark"), Format(urls_color, "http://oshpark.com")])
+	response = Format("Laen's PCB order has its own channel, %s and the website is %s" % [Format(:yellow, "#oshpark"), urlize("http://oshpark.com")])
 	m.reply(response)
 end
 
 bot.on :message, /^!repo/ do |m|
-	response = Format("Add to or modify me: %s" % [Format(:urls_color, "https://github.com/x37v/dorkbotpdx_bot")])
+	response = Format("Add to or modify me: %s" % [urlize("https://github.com/x37v/dorkbotpdx_bot")])
 	m.reply(response)
 end
 
 bot.on :message, /^!faq/ do |m|
-	response = Format("DorkbotPDX FAQ lives here: %s" % [Format(:urls_color, "http://dorkbotpdx.org/wiki/frequently_asked_questions")])
+	response = Format("DorkbotPDX FAQ lives here: %s" % [urlize("http://dorkbotpdx.org/wiki/frequently_asked_questions")])
 	m.reply(response)
 end
 
 bot.on :message, /^!meet/ do |m|
-	response = Format("Meetings are every other Monday, 7pm, at Backspace:  %s" % [Format(:urls_color, "http://dorkbotpdx.org/meetings")])
+	response = Format("Meetings are every other Monday, 7pm, at Backspace:  %s" % [urlize("http://dorkbotpdx.org/meetings")])
 	m.reply(response)
+end
+
+def urlize(u)
+	return Format(:urls_color, u)
 end
 
 bot.start
