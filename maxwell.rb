99ddc15f5be81538f5f569545f0473a6e57eb3ac
Alex Norman <x37v.alex@gmail.com>
2013-04-07 12:12:28 -0700

    inline formatting, more ruby like..

--- maxwell.rb
+++ maxwell.rb
@@ -15,28 +15,28 @@ bot = create_bot(opts)
 #will execute the block given when a 'message' with prefix '!repo' is sent
 #the next string is put into the help system for this command
 bot.on_with_help :message, 'repo', 'Gives information about how to extend/enhance my programming' do |m|
-  response = Format("Add to or modify me: %s" % [urlize("https://github.com/x37v/dorkbotpdx_bot")])
+  response = Format("Add to or modify me: #{urlize("https://github.com/x37v/dorkbotpdx_bot")}")
   m.reply(response)
 end
 
 #responds to !paul or !teensy or !pjrc
 bot.on_with_help :message, '(paul|teensy|pjrc)', 'Gives information about Paul/Teensy/pjrc' do |m|
-  response = Format("Paul [teensy]'s website is %s and he can be reached at %s" % [urlize("https://www.pjrc.com"), Format(:yellow, "paul@pjrc.com")])
+  response = Format("Paul [teensy]'s website is #{urlize("https://www.pjrc.com")} and he can be reached at #{Format(:yellow, "paul@pjrc.com")}")
   m.reply(response)
 end
 
 bot.on_with_help :message, '(laen|osh|pcb)', 'Gives information Laen and #oshcpark PCB orders' do |m|
-  response = Format("Laen's PCB order has its own channel, %s and the website is %s" % [Format(:yellow, "#oshpark"), urlize("http://oshpark.com")])
+  response = Format("Laen's PCB order has its own channel, #{Format(:yellow, "#oshpark")} and the website is #{urlize("http://oshpark.com")}")
   m.reply(response)
 end
 
 bot.on_with_help :message, 'faq', 'Provides a link to the FAQ (frequently asked questions)' do |m|
-  response = Format("DorkbotPDX FAQ lives here: %s" % [urlize("http://dorkbotpdx.org/wiki/frequently_asked_questions")])
+  response = Format("DorkbotPDX FAQ lives here: #{urlize("http://dorkbotpdx.org/wiki/frequently_asked_questions")}")
   m.reply(response)
 end
 
 bot.on_with_help :message, 'meet', 'Describes when we meet and links to the meeting page' do |m|
-  response = Format("Meetings are every other Monday, 7pm, at Backspace: %s" % [urlize("http://dorkbotpdx.org/meetings")])
+  response = Format("Meetings are every other Monday, 7pm, at Backspace: #{urlize("http://dorkbotpdx.org/meetings")}")
   m.reply(response)
 end
 
