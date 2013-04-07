d97a443018e14f90b563ec0ee6844943fef08ed7
Alex Norman <x37v.alex@gmail.com>
2013-04-07 11:54:37 -0700

    retabbed, inline help system start..

--- bot.rb
+++ bot.rb
@@ -4,46 +4,92 @@
 require 'open-uri'
 require 'cinch'
 [
-	'seen',
-	'hello',
-	'topic_history',
-	'memo',
+  'seen',
+  'hello',
+  'topic_history',
+  'memo',
 ].each do |p|
-	require File.join(File.dirname(__FILE__), "plugins", p)
+  require File.join(File.dirname(__FILE__), "plugins", p)
 end
 
 # Automatically shorten URL's found in messages
 # Using the tinyURL API
 
-def create_bot(opts)
-	Cinch::Bot.new do
-	  configure do |c|
-		 c.server = opts[:server]
-		 c.channels = opts[:channels]
-		 c.nick = opts[:name]
-		 c.plugins.plugins = [Seen, Hello, TopicHistory, Memo]
-	  end
+class Cinch::Bot
+  #very basic help system
+  def add_help(cmd, help_text)
+    @help_data ||= Hash.new
+    @help_data[cmd] = help_text
+  end
 
-	  helpers do
-		 def shorten(url)
-			url = open("http://tinyurl.com/api-create.php?url=#{URI.escape(url)}").read
-			url == "Error" ? nil : url
-		 rescue OpenURI::HTTPError
-			nil
-		 end
-	  end
+  def on_with_help(event, cmd, help_text, &blk)
+    add_help(cmd, help_text)
+    self.on(event, /!#{cmd}/, &blk)
+  end
 
-	  on :channel do |m|
-		 return if m =~ /\A!/
-		 urls = URI.extract(m.message, "http").reject { |url| url.length < 70 }
-
-		 if urls.any?
-			short_urls = urls.map {|url| shorten(url) }.compact
-
-			unless short_urls.empty?
-			  m.reply short_urls.join(", ")
-			end
-		 end
-	  end
-	end
+  def print_help(message)
+    message.reply(Format(:red, "-----------------------------------------------------------------"))
+    message.reply(Format("I am %s. I am programmed with the following commands:" % [Format(:yellow, "Maxwell")]))
+    if @help_data
+      message.reply(Format(:red, "------------------- public/channel commands ---------------------"))
+      @help_data.each do |key, value|
+        message.reply(Format(" %s : %s" % [Format(:pink, "%-6s" % ['!' + key]), value]))
+      end
+      message.reply(Format(:red, "------------------- private/message commands --------------------"))
+      message.reply(Format(" %s : That's all we've built so far!" % [Format(:pink, '/msg %s help')]))
+    end
+  end
 end
+
+def create_bot(opts)
+  Cinch::Bot.new do
+    configure do |c|
+      c.server = opts[:server]
+      c.channels = opts[:channels]
+      c.nick = opts[:name]
+      c.plugins.plugins = [Seen, Hello, TopicHistory, Memo]
+    end
+
+    helpers do
+      def shorten(url)
+        url = open("http://tinyurl.com/api-create.php?url=#{URI.escape(url)}").read
+        url == "Error" ? nil : url
+      rescue OpenURI::HTTPError
+        nil
+      end
+
+      def reply_cmd(m, cmd, description)
+        return m.reply(Format(" %s : %s" % [Format(:pink, "%-6s" % ['!' + cmd]), description]))
+      end
+
+      def urlize(u)
+        return Format(:lime, u)
+      end
+    end
+
+    on :channel do |m|
+      return if m =~ /\A!/
+        urls = URI.extract(m.message, "http").reject { |url| url.length < 70 }
+
+      if urls.any?
+        short_urls = urls.map {|url| shorten(url) }.compact
+
+        unless short_urls.empty?
+          m.reply short_urls.join(", ")
+        end
+      end
+    end
+
+    #reply to help commands with our help system
+    on :message, /^!?help/ do|m|
+      if (not m.channel?) # sent with a private message
+        bot.print_help(m)
+      elsif (m.message =~ /^!help/) # asked in the channel
+        response = Format("Maxwell at your service. I am here to assist you.  %s" % [Format(:pink, "/msg %s help" % [opts[:name]])])
+        m.reply(response)
+      end
+    end
+  end
+end
+
+
--- maxwell.rb
+++ maxwell.rb
@@ -3,63 +3,41 @@
 require File.join(File.dirname(__FILE__), 'bot')
 
 opts = {
-	:server => 'irc.freenode.org',
-	:channels => ['#dorkbotpdx'],
-	:name => 'maxwell-dbpdx'
+  :server => 'irc.freenode.org',
+  :channels => ['#dorkbotpdx'],
+  :name => 'maxwell-dbpdx'
 }
 
 bot = create_bot(opts)
 
-bot.on :message, /^!?help/ do |m|
-	if(not m.channel?) # sent with a private message
-		m.reply(Format(:red, "-----------------------------------------------------------------"))
-		m.reply(Format("I am %s. I am programmed with the following commands:" % [Format(:yellow, "Maxwell")]))
-		m.reply(Format(:red, "------------------- public/channel commands ---------------------"))
-		reply_cmd(m, 'paul', 'Gives information about Paul/Teensy/pjrc') 
-		reply_cmd(m, 'laen', 'Gives information Laen and #oshcpark PCB orders')
-		reply_cmd(m, 'faq', 'Provides a link to the FAQ (frequently asked questions)')
-		reply_cmd(m, 'meet', 'Describes when we meet and links to the meeting page')
-		reply_cmd(m, 'repo', 'Gives information about how to extend/enhance my programming.')
-		reply_cmd(m, 'help', 'Tells users how to get help from me.')
-		m.reply(Format(:red, "------------------- private/message commands --------------------"))
-		m.reply(Format(" %s : That's all we've built so far!" % [Format(:pink, '/msg %s help')]))
-	elsif(m.message =~ /^!help/) # asked in the channel
-		response = Format("Maxwell at your service. I am here to assist you.  %s" % [Format(:pink, "/msg %s help" % [opts[:name]])])
-		m.reply(response)
-	end
+#check out bot.rb for 'helpers' that you can use
+
+#will execute the block given when a 'message' with prefix '!repo' is sent
+#the next string is put into the help system for this command
+bot.on_with_help :message, 'repo', 'Gives information about how to extend/enhance my programming' do |m|
+  response = Format("Add to or modify me: %s" % [urlize("https://github.com/x37v/dorkbotpdx_bot")])
+  m.reply(response)
 end
 
-bot.on :message, /^!(paul|teensy|pjrc)/ do |m|
-	response = Format("Paul [teensy]'s website is %s and he can be reached at %s" % [urlize("https://www.pjrc.com"), Format(:yellow, "paul@pjrc.com")])
-	m.reply(response)
+#responds to !paul or !teensy or !pjrc
+bot.on_with_help :message, '(paul|teensy|pjrc)', 'Gives information about Paul/Teensy/pjrc' do |m|
+  response = Format("Paul [teensy]'s website is %s and he can be reached at %s" % [urlize("https://www.pjrc.com"), Format(:yellow, "paul@pjrc.com")])
+  m.reply(response)
 end
 
-bot.on :message, /^!(laen|osh|pcb)/ do |m|
-	response = Format("Laen's PCB order has its own channel, %s and the website is %s" % [Format(:yellow, "#oshpark"), urlize("http://oshpark.com")])
-	m.reply(response)
+bot.on_with_help :message, '(laen|osh|pcb)', 'Gives information Laen and #oshcpark PCB orders' do |m|
+  response = Format("Laen's PCB order has its own channel, %s and the website is %s" % [Format(:yellow, "#oshpark"), urlize("http://oshpark.com")])
+  m.reply(response)
 end
 
-bot.on :message, /^!repo/ do |m|
-	response = Format("Add to or modify me: %s" % [urlize("https://github.com/x37v/dorkbotpdx_bot")])
-	m.reply(response)
+bot.on_with_help :message, 'faq', 'Provides a link to the FAQ (frequently asked questions)' do |m|
+  response = Format("DorkbotPDX FAQ lives here: %s" % [urlize("http://dorkbotpdx.org/wiki/frequently_asked_questions")])
+  m.reply(response)
 end
 
-bot.on :message, /^!faq/ do |m|
-	response = Format("DorkbotPDX FAQ lives here: %s" % [urlize("http://dorkbotpdx.org/wiki/frequently_asked_questions")])
-	m.reply(response)
-end
-
-bot.on :message, /^!meet/ do |m|
-	response = Format("Meetings are every other Monday, 7pm, at Backspace:  %s" % [urlize("http://dorkbotpdx.org/meetings")])
-	m.reply(response)
-end
-
-def reply_cmd(m, cmd, description)
-	return m.reply(Format(" %s : %s" % [Format(:pink, "%-6s" % ['!' + cmd]), description]))
-end
-
-def urlize(u)
-	return Format(:lime, u)
+bot.on_with_help :message, 'meet', 'Describes when we meet and links to the meeting page' do |m|
+  response = Format("Meetings are every other Monday, 7pm, at Backspace: %s" % [urlize("http://dorkbotpdx.org/meetings")])
+  m.reply(response)
 end
 
 bot.start
